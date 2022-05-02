655e28306b9d51c5b74ca314c0c7a9a0ab4008b4
Viktoria <zaharova181114@gmail.com>
2022-05-02 22:20:13 +0300

    Initial commit

---
+++ app/js/common.js
@@ -0,0 +1,12 @@
+$('.clients-slider').slick({
+    slidesToShow: 4,
+    prevArrow: '<button type="button" class="slick-prev"></button>',
+    nextArrow: '<button type="button" class="slick-next"></button>'
+});
+
+$('.home-slider').slick({
+    slidesToShow: 1,
+    fade: true,
+    prevArrow: '<button type="button" class="slick-prev"></button>',
+    nextArrow: '<button type="button" class="slick-next"></button>'
+});
