af8d0346652c0af8fa94555c77d45a250d464bae
Viktoria <zaharova181114@gmail.com>
2022-05-03 22:00:18 +0300

    add blocks

--- app/js/common.js
+++ app/js/common.js
@@ -10,3 +10,40 @@ $('.home-slider').slick({
     prevArrow: '<button type="button" class="slick-prev"></button>',
     nextArrow: '<button type="button" class="slick-next"></button>'
 });
+
+$(window).scroll(function () {
+    if ($(this).scrollTop() > 52) {
+        $('header').addClass('fixed');
+        $('body').addClass('header-fixed');
+    } else {
+        $('header').removeClass('fixed');
+        $('body').removeClass('header-fixed');
+    }
+});
+
+
+$('.form-search input').on('keyup change', function() {
+    if (this.value.length > 0) {
+        $('.search-dropdown').fadeIn();
+    } else {
+        $('.search-dropdown').fadeOut();
+    }
+});
+
+
+$('.location-links').on('click', function (e) {
+    e.preventDefault();
+    $('.location-dropdown').fadeToggle();
+});
+
+$('.location-dropdown__close').on('click', function (e) {
+    e.preventDefault();
+    $('.location-dropdown').fadeOut();
+});
+
+$('.btn-burger').on('click', function (e) {
+    e.preventDefault();
+    $(this).addClass('trigger');
+    $('.header-bottom').css('display','flex');
+});
+
